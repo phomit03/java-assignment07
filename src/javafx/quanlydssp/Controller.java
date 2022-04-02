c921adbbc3e325a7aa3c2356cc9ac77881ef470d
TharoXNguyen <96766829+phomit03@users.noreply.github.com>
2022-04-02 15:59:10 +0700

    update code

--- src/javafx/quanlydssp/Controller.java
+++ src/javafx/quanlydssp/Controller.java
@@ -1,5 +1,33 @@
 package javafx.quanlydssp;
 
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
+    public Text result;
+    public TextField id;
+    public TextField name;
+    public TextField unit;
+    public TextField price;
+    public TextField quantity;
+
+    public void addProducts(){
+        try {
+            Products p = new Products(id.getText(), name.getText(), unit.getText(),
+                    Float.parseFloat(price.getText()), Integer.parseInt(quantity.getText()));
+
+            result.setText(result.getText() + "\t" + p.inputProducts());
+
+            id.setText("");
+            name.setText("");
+            unit.setText("");
+            price.setText("");
+            quantity.setText("");
+
+        }
+        catch (Exception e){
+
+        }
+    }
 
 }
